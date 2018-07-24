76ab72272aea835dd0e4aad62f28f4ba52c8a5a7
christos dousmanopoulos <christos_dousmanopoulos@yahoo.gr>
2018-07-24 10:59:19 +0300

    initial commit

---
+++ read_json.py
@@ -0,0 +1,19 @@
+import json
+file = open('reci.json','r')
+data = json.loads(file.read())
+
+splitted_data=[]
+for i in data:
+    for y in i['ingredients']:
+        if "eggs" in y or "egg" in y :
+            print('The recipie with id ', i['id'] , 'has eggs')
+
+# for datas in load_data:
+#     print(datas)
+
+
+# with open("reci.json","r") as json_file:
+#     data = json.load(json_file)
+#     print(data['id'])
+    
+    
