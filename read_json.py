60d7bf845d09f4f674665ee29711bf9e33e48c7d
christos dousmanopoulos <christos_dousmanopoulos@yahoo.gr>
2018-08-03 10:14:39 +0300

    finds only the exact matches and exports in a txt file

--- read_json.py
+++ read_json.py
@@ -1,12 +1,17 @@
-import json
+import json,re
 file = open('recipes.json','r')
 data = json.loads(file.read())
 
-splitted_data=[]
+
+all_data = []
 for i in data:
     for y in i['ingredients']:
-        if "eggs" in y or "egg" in y :
-            print('The recipie with id ', i['id'] , 'has eggs')
-            #test
-    
-    
+        matching_eggs = re.search(r'\beggs\b',y)
+        matching_egg = re.search(r'\begg\b',y)
+        if matching_eggs or matching_egg:
+            string ="The recipie with id {} has eggs".format(i['id']) 
+            print(string)
+            # all_data.append(str(string))
+            # output = open('log.txt','w')
+            # for recipies in all_data:
+            #     output.write(recipies + "\n")
